983564c83b91ca2b02b60fac6662181bb014f615
shhong7757 <ghdtkdgur123@gmail.com>
2024-02-16 00:34:43 +0900

    Initial Commit

---
+++ pomodoro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pomodoro
+//
+//  Created by sang hyeok hong on 2/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
